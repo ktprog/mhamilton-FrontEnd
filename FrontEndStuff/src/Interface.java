b1a0a5157d6d45f965abbd2e171d27f2eda6af0d
workm <workm@DESKTOP-EOUG9I5>
2021-03-08 10:08:47 -0500

    Test

--- FrontEndStuff/src/Interface.java
+++ FrontEndStuff/src/Interface.java
@@ -3,7 +3,7 @@ public class Interface {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Let's GOoooooooooo");
+		System.out.println("Let's GOooooooooooo");
 
 	}
 
